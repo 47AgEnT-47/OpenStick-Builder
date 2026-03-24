a0541ffe6994324d6bab454434bb988c35cbb86b
47AgEnT-47 <251953826+47AgEnT-47@users.noreply.github.com>
2026-03-24 16:28:27 +0700

    Update debootstrap.sh

--- scripts/debootstrap.sh
+++ scripts/debootstrap.sh
@@ -109,5 +109,3 @@ mkdir -p ${CHROOT}/lib/firmware/msm-firmware-loader
 # update fstab
 echo "PARTUUID=80780b1d-0fe1-27d3-23e4-9244e62f8c46\t/boot\text2\tdefaults\t0 2" > ${CHROOT}/etc/fstab
 
-# backup rootfs
-tar cpzf rootfs.tgz --exclude="usr/bin/qemu-aarch64-static" -C rootfs .
