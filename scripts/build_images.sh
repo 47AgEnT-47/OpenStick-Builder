17394b0b7ad81d3858bd73aeb25f051089f87419
47AgEnT-47 <251953826+47AgEnT-47@users.noreply.github.com>
2026-03-24 16:48:36 +0700

    Update build_images.sh

--- scripts/build_images.sh
+++ scripts/build_images.sh
@@ -2,19 +2,17 @@
 
 CHROOT=${CHROOT=$(pwd)/rootfs}
 
-#package rootfs
+# package rootfs
 rm -f rootfs.raw boot.raw
 mkdir -p files mnt
 
 # create boot
-truncate -s 67108864 boot.raw
 mkfs.ext2 boot.raw
 mount boot.raw mnt
 tar xf rootfs.tgz -C mnt ./boot --exclude='./boot/linux.efi' --strip-components=2
 umount mnt
 
 # create root img
-truncate -s 1610612736 rootfs.raw
 mkfs.ext4 rootfs.raw
 mount rootfs.raw mnt
 tar xpf rootfs.tgz -C mnt --exclude='./boot/*' --exclude='./root/*' --exclude='./dev/*'
@@ -29,6 +27,12 @@ rm -rf mnt/usr/include mnt/usr/lib/aarch64-linux-gnu/pkgconfig mnt/usr/lib/*.a m
 
 umount mnt
 
-# create sparse android images 
+# resize to minimum
+e2fsck -f rootfs.raw
+resize2fs -M rootfs.raw
+e2fsck -f boot.raw
+resize2fs -M boot.raw
+
+# create sparse android images
 img2simg rootfs.raw files/rootfs.bin
 img2simg boot.raw files/boot.bin
