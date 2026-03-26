ec9c51d6907462285f8a8ef7edb0acd7b954f96c
47AgEnT-47 <251953826+47AgEnT-47@users.noreply.github.com>
2026-03-27 01:46:50 +0700

    Update debootstrap.sh

--- scripts/debootstrap.sh
+++ scripts/debootstrap.sh
@@ -1,114 +1,58 @@
 #!/bin/sh -e
 
-CHROOT=${CHROOT=$(pwd)/rootfs}
-RELEASE=${RELEASE=stable}
-HOST_NAME=${HOST_NAME=openstick}
+: "${CHROOT:=$(pwd)/rootfs}" "${RELEASE:=stable}" "${HOST_NAME:=openstick}"
 
-rm -rf ${CHROOT}
+rm -rf "$CHROOT"
 
-# Use mmdebstrap for faster builds (god, it's so much faster)
-echo "Using mmdebstrap for fast bootstrap..."
-mmdebstrap --arch=arm64 \
-    --include=systemd,udev,dbus,apt,wget,ca-certificates \
-    --keyring=/usr/share/keyrings/debian-archive-keyring.gpg \
-    ${RELEASE} ${CHROOT}
+echo "Using mmdebstrap..."
+mmdebstrap --arch=arm64 --include=systemd,udev,dbus,apt,wget,ca-certificates \
+    --keyring=/usr/share/keyrings/debian-archive-keyring.gpg "$RELEASE" "$CHROOT"
 
-cat << EOF > ${CHROOT}/etc/apt/sources.list
-deb http://deb.debian.org/debian ${RELEASE} main contrib non-free-firmware
-deb http://deb.debian.org/debian-security/ ${RELEASE}-security main contrib non-free-firmware
-deb http://deb.debian.org/debian ${RELEASE}-updates main contrib non-free-firmware
-EOF
+# Объединяем создание файлов через tee
+printf "deb http://deb.debian.org/debian %s main contrib non-free-firmware\ndeb http://deb.debian.org/debian-security/ %s-security main contrib non-free-firmware\ndeb http://deb.debian.org/debian %s-updates main contrib non-free-firmware\n" "$RELEASE" "$RELEASE" "$RELEASE" > "$CHROOT/etc/apt/sources.list"
 
-# Speed up apt
-cat << EOF > ${CHROOT}/etc/apt/apt.conf.d/99speedup
+cat << EOF > "$CHROOT/etc/apt/apt.conf.d/99speedup"
 APT::Acquire::Retries "3";
-APT::Acquire::http::Timeout "10";
-APT::Acquire::ftp::Timeout "10";
+APT::Acquire::{http,ftp}::Timeout "10";
 Acquire::Languages "none";
-APT::Install-Recommends "false";
-APT::Install-Suggests "false";
-DPkg::Options::="--force-confdef";
-DPkg::Options::="--force-confold";
+APT::Install-Re{commends,ggests} "false";
+DPkg::Options::="--force-conf{def,old}";
 EOF
 
-mount -t proc proc ${CHROOT}/proc/
-mount -t sysfs sys ${CHROOT}/sys/
-mount -o bind /dev/ ${CHROOT}/dev/
-mount -o bind /dev/pts/ ${CHROOT}/dev/pts/
-mount -o bind /run ${CHROOT}/run/
+for m in proc sys dev dev/pts run; do mount --bind "/$m" "$CHROOT/$m" 2>/dev/null || mount -t "${m%/}" "${m%/}" "$CHROOT/$m"; done
 
-# configs'n stuff
-mkdir -p ${CHROOT}/etc/systemd/system
-cp -a configs/system/* ${CHROOT}/etc/systemd/system
-cp configs/nftables.conf ${CHROOT}/etc/nftables.conf
-mkdir -p ${CHROOT}/etc/NetworkManager/system-connections
-mkdir -p ${CHROOT}/etc/NetworkManager/conf.d
-cp configs/*.nmconnection ${CHROOT}/etc/NetworkManager/system-connections
-chmod 0600 ${CHROOT}/etc/NetworkManager/system-connections/*
-cp configs/99-custom.conf ${CHROOT}/etc/NetworkManager/conf.d/
+# Копирование конфигов (объединяем mkdir и cp)
+mkdir -p "$CHROOT/etc/systemd/system" "$CHROOT/etc/NetworkManager/system-connections" "$CHROOT/etc/NetworkManager/conf.d"
+cp -a configs/system/* "$CHROOT/etc/systemd/system/"
+cp configs/nftables.conf "$CHROOT/etc/nftables.conf"
+cp configs/*.nmconnection "$CHROOT/etc/NetworkManager/system-connections/"
+chmod 0600 "$CHROOT/etc/NetworkManager/system-connections/"*
+cp configs/99-custom.conf "$CHROOT/etc/NetworkManager/conf.d/"
+cp configs/install_dnsproxy.sh scripts/setup.sh /usr/bin/qemu-aarch64-static "$CHROOT/"
 
-# chroot setup
-cp configs/install_dnsproxy.sh ${CHROOT}
-cp scripts/setup.sh ${CHROOT}
+chroot "$CHROOT" /usr/bin/qemu-aarch64-static /bin/sh -c "/setup.sh"
 
-# Copy qemu static and run setup script in chroot
-cp /usr/bin/qemu-aarch64-static ${CHROOT}/usr/bin/
-chroot ${CHROOT} qemu-aarch64-static /bin/sh -c "/setup.sh"
+for a in proc sys dev/pts dev run; do umount "$CHROOT/$a"; done
 
-# cleanup
-for a in proc sys dev/pts dev run; do
-    umount ${CHROOT}/${a}
-done;
+rm -f "$CHROOT/install_dnsproxy.sh" "$CHROOT/setup.sh" "$CHROOT/qemu-aarch64-static"
+: > "$CHROOT/root/.bash_history"
 
-rm ${CHROOT}/install_dnsproxy.sh
-rm -f ${CHROOT}/setup.sh
-rm -f ${CHROOT}/usr/bin/qemu-aarch64-static
-echo -n > ${CHROOT}/root/.bash_history
+echo "$HOST_NAME" > "$CHROOT/etc/hostname"
+sed -i "/localhost/ s/$/ $HOST_NAME/" "$CHROOT/etc/hosts"
+printf "\n192.168.100.1\t%s\n" "$HOST_NAME" >> "$CHROOT/etc/hosts"
 
-echo ${HOST_NAME} > ${CHROOT}/etc/hostname
-sed -i "/localhost/ s/$/ ${HOST_NAME}/" ${CHROOT}/etc/hosts
+cp -a configs/dhcp.conf "$CHROOT/etc/dnsmasq.d/dhcp.conf"
+cp -a configs/rc.local "$CHROOT/etc/rc.local" && chmod +x "$CHROOT/etc/rc.local"
+cp -a configs/msm8916-usb-gadget.{sh,conf} "$CHROOT/usr/sbin/" 2>/dev/null || (cp configs/msm8916-usb-gadget.sh "$CHROOT/usr/sbin/" && cp configs/msm8916-usb-gadget.conf "$CHROOT/etc/")
 
-# setup dnsmasq
-cp -a configs/dhcp.conf ${CHROOT}/etc/dnsmasq.d/dhcp.conf
+mkdir -p "$CHROOT/etc/hostapd" "$CHROOT/boot/extlinux" "$CHROOT/lib/firmware/msm-firmware-loader"
+cp configs/hostapd.conf "$CHROOT/etc/hostapd/"
+cp configs/wifi-ap.sh scripts/msm-firmware-loader.sh "$CHROOT/usr/sbin/" && chmod +x "$CHROOT/usr/sbin/wifi-ap.sh"
 
-# hosts entry for the LAN IP
-cat <<EOF >> ${CHROOT}/etc/hosts
+wget -O - https://github.com/Mio-sha512/openstick-stuff/raw/refs/heads/main/builder-stuff/linux-postmarketos-qcom-msm8916-6.12.1-cpr.apk | tar xkzf - -C "$CHROOT" --exclude=.PKGINFO --exclude=.SIGN* 2>/dev/null
 
-192.168.100.1	${HOST_NAME}
-EOF
+cp configs/extlinux.conf "$CHROOT/boot/extlinux/"
+rm -rf "$CHROOT/boot/dtbs/qcom/*" && cp dtbs/* "$CHROOT/boot/dtbs/qcom/"
 
-# add rc-local
-cp -a configs/rc.local ${CHROOT}/etc/rc.local
-chmod +x ${CHROOT}/etc/rc.local
-
-# add MSM8916 USB gadget
-cp -a configs/msm8916-usb-gadget.sh ${CHROOT}/usr/sbin/
-cp configs/msm8916-usb-gadget.conf ${CHROOT}/etc/
-
-# setup WiFi AP with hostapd
-mkdir -p ${CHROOT}/etc/hostapd
-cp configs/hostapd.conf ${CHROOT}/etc/hostapd/
-cp configs/wifi-ap.sh ${CHROOT}/usr/sbin/
-chmod +x ${CHROOT}/usr/sbin/wifi-ap.sh
-
-cp -a scripts/msm-firmware-loader.sh ${CHROOT}/usr/sbin
-
-# install kernel
-wget -O - https://github.com/Mio-sha512/openstick-stuff/raw/refs/heads/main/builder-stuff/linux-postmarketos-qcom-msm8916-6.12.1-cpr.apk \
-    | tar xkzf - -C ${CHROOT} --exclude=.PKGINFO --exclude=.SIGN* 2>/dev/null
-
-mkdir -p ${CHROOT}/boot/extlinux
-cp configs/extlinux.conf ${CHROOT}/boot/extlinux
-
-# copy custom dtb's
-rm -rf ${CHROOT}/boot/dtbs/qcom/*
-cp dtbs/* ${CHROOT}/boot/dtbs/qcom/
-
-# create missing directory
-mkdir -p ${CHROOT}/lib/firmware/msm-firmware-loader
-
-# update fstab
-echo "PARTUUID=80780b1d-0fe1-27d3-23e4-9244e62f8c46\t/boot\text2\tdefaults\t0 2" > ${CHROOT}/etc/fstab
-
-# backup rootfs
+echo "PARTUUID=80780b1d-0fe1-27d3-23e4-9244e62f8c46\t/boot\text2\tdefaults\t0 2" > "$CHROOT/etc/fstab"
 tar cpzf rootfs.tgz --exclude="usr/bin/qemu-aarch64-static" -C rootfs .
