251bf3ff5cf6f1c2cdd4a4a63e459ee15b209c39
47AgEnT-47 <251953826+47AgEnT-47@users.noreply.github.com>
2026-03-28 01:29:47 +0700

    Update wifi-ap.sh

--- configs/wifi-ap.sh
+++ configs/wifi-ap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
+nmcli dev set wlan0 managed no
 systemctl stop wpa_supplicant
 systemctl mask wpa_supplicant
 systemctl unmask hostapd
 nmcli con down wlan0 2>/dev/null
-nmcli dev set wlan0 managed no
-systemctl start hostapd
+systemctl restart hostapd
