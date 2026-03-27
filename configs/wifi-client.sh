bede704a0e596ffcba6c2b363048b948d80afb73
47AgEnT-47 <251953826+47AgEnT-47@users.noreply.github.com>
2026-03-28 01:30:33 +0700

    Update wifi-client.sh

--- configs/wifi-client.sh
+++ configs/wifi-client.sh
@@ -2,6 +2,6 @@
 systemctl stop hostapd
 systemctl mask hostapd
 systemctl unmask wpa_supplicant
-systemctl start wpa_supplicant
+systemctl restart wpa_supplicant
 nmcli dev set wlan0 managed yes
 nmcli con up wlan0
