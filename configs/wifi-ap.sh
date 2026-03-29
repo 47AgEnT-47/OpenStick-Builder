33afe4694082ed3a865819a740afeaf66c5c3885
47AgEnT-47 <251953826+47AgEnT-47@users.noreply.github.com>
2026-03-29 19:14:06 +0700

    Update wifi-ap.sh

--- configs/wifi-ap.sh
+++ configs/wifi-ap.sh
@@ -4,5 +4,4 @@ nmcli connection add type wifi ifname wlan0 con-name hotspot autoconnect yes ssi
 nmcli connection modify hotspot 802-11-wireless-security.key-mgmt wpa-psk
 nmcli connection modify hotspot 802-11-wireless-security.psk "12345678"
 nmcli connection modify hotspot ipv4.method shared
-nmcli connection modify hotspot ipv4.shared-interface usb0
 nmcli connection up hotspot
